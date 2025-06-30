3e600dd6d69fa2269e6ffc0c46dfa8f0d72cf167
sakitibi <sakitibi10nin@icloud.com>
2025-06-30 21:59:51 +0900

    aaa

---
+++ README.md
@@ -0,0 +1,8 @@
+### 引数 /update &lt;filetype&gt; &lt;version&gt;
+#### 注意　fabricで作られています
+#### このMod PluginはSKNewRolesに組み込む前提です
+#### SKNewRolesを持っていない方は<a href="https://sakitibi.github.io/13nin.com/SKNewRoles">ここ</a>からダウンロードして下さい
+#### .7zファイルだけはダウンロードするだけですので
+#### ご自身で解凍して下さい
+#### 対応ファイルタイプ zip 7z xz gz bz2
+#### 利用可能バージョン(定期的に変わる) 2.5.0.7 2.5.0.8 2.5.0.9 2.6.0.0 2.9.9.9 3.0.0.0
---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.8.22"
+    id("fabric-loom") version "1.2.8"
+}
+
+group = "patch.plugins"
+version = "3.0.0.0"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.maven.apache.org/maven2") }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.19.4")
+    mappings("net.fabricmc:yarn:1.19.4+build.1:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.14.21")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.76.0+1.19.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.apache.commons:commons-compress:1.26.1")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
