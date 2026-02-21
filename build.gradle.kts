6eec45ee0cc67707f52d5aa4e8b6dd42b3bd5e01
sakitibi <sakitibi10nin@icloud.com>
2026-02-21 23:59:22 +0900

    Merge remote changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ dependencies {
     mappings("net.fabricmc:yarn:1.19.4+build.1:v2")
     modImplementation("net.fabricmc:fabric-loader:0.14.21")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.87.2+1.19.4")
+    modImplementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.apache.commons:commons-compress:1.26.1")
 }
 
