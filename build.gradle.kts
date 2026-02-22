73bb5b8fbcb96d2d9f3d445c883411257e58520c
sakitibi <sakitibi10nin@icloud.com>
2026-02-22 15:46:02 +0900

    Merge remote changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 import net.fabricmc.loom.task.RemapJarTask
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm") version "1.8.22"
     id("fabric-loom") version "1.2.8"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "patch.plugins"
@@ -21,37 +19,17 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.18.4")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.87.2+1.19.4")
 
+    // 読み込みつつ、Jarにも同梱（include）する
     modImplementation("com.squareup.okhttp3:okhttp:4.11.0")
+    include("com.squareup.okhttp3:okhttp:4.11.0")
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    include("org.jetbrains.kotlin:kotlin-stdlib")
+
     implementation("org.apache.commons:commons-compress:1.26.1")
+    include("org.apache.commons:commons-compress:1.26.1")
 }
 
 kotlin {
     jvmToolchain(17)
-}
-
-tasks {
-    // ShadowJar: FatJar 作成
-    withType<ShadowJar>().configureEach {
-        archiveClassifier.set("")
-        mergeServiceFiles()
-        from(project.configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-        finalizedBy("remapJar")
-    }
-
-    // 標準 jar は shadowJar に依存
-    named("jar") {
-        dependsOn("shadowJar")
-    }
-
-    // remapJar は ShadowJar を元に Fabric 用に変換
-    withType<RemapJarTask>().configureEach {
-        dependsOn("shadowJar")
-        // ShadowJar の archiveFile を型安全に参照
-        input.set(tasks.withType<ShadowJar>().map { it.archiveFile.get() })
-
-        doFirst {
-            println("Remapping FatJar for Fabric…")
-        }
-    }
 }
